6ae838bac469a504ebf81d93bdb47005cf453c3b
dtian118 <dziyangtian@gmail.com>
2024-12-31 15:44:39 -0500

    add stage 2

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # SDA-bootcamp-project
 
-Stage 1 - Basic Chatbot
+Stage 2 - Basic Chatbot with FastAPI
 
-A basic chatbot using streamlit and openai api.
+A basic chatbot using streamlit and openai api. At this stage we move the call to openai to the backend using FastAPI
 
-Store your `OPENAI_API_KEY` in `.env` file. Run
+Store your `OPENAI_API_KEY` in `.env` file.
 
+Start the backend app first using:
+
+```
+uvicorn backend:app --reload
+```
+
+And then use 
 ```
 streamlit run chatbot.py
 ```
-to run the app.
+to run the streamlit app. Make sure that always start the backend first!
--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,13 @@ from openai import OpenAI
 import streamlit as st
 import os 
 from dotenv import load_dotenv
+import requests
 
 st.title("Chatbot basic")
 
 load_dotenv()
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+chat_url = "http://127.0.0.1:8000/chat/"
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -25,13 +26,27 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
+
+        payload = {
+            "messages": [
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
+            ]
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        # No Stream approach
+        # stream = requests.post(chat_url, json=payload, headers=headers)
+        # response = stream.json()["reply"]
+        # st.markdown(response)
+
+        # Stream approach
+        def get_stream_response():
+            with requests.post(chat_url, json=payload, headers=headers, stream=True) as r:
+                for chunk in r:
+                    yield chunk.decode('utf-8')
+        response = st.write_stream(get_stream_response)
+
     st.session_state.messages.append({"role": "assistant", "content": response})
