a791b21af298779728dbbb41dd6ae7ab41a7716b
davyzhang3 <davyzhang325@gmail.com>
2025-01-27 00:41:30 +0000

    fix: udpate backend port number

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,12 @@ import uuid
 import requests
 
 # Backend URLs define
-LOAD_CHAT_URL = "http://127.0.0.1:8000/load_chat/"
-SAVE_CHAT_URL = "http://127.0.0.1:8000/save_chat/"
-DELETE_CHAT_URL = "http://127.0.0.1:8000/delete_chat/"
-UPLOAD_PDF_URL = "http://127.0.0.1:8000/upload_pdf/"
-CHAT_URL = "http://127.0.0.1:8000/chat/"
-RAG_CHAT_URL = "http://127.0.0.1:8000/rag_chat/"
+LOAD_CHAT_URL = "http://127.0.0.1:5000/load_chat/"
+SAVE_CHAT_URL = "http://127.0.0.1:5000/save_chat/"
+DELETE_CHAT_URL = "http://127.0.0.1:5000/delete_chat/"
+UPLOAD_PDF_URL = "http://127.0.0.1:5000/upload_pdf/"
+CHAT_URL = "http://127.0.0.1:5000/chat/"
+RAG_CHAT_URL = "http://127.0.0.1:5000/rag_chat/"
 
 # Initialize session state
 if "history_chats" not in st.session_state:
