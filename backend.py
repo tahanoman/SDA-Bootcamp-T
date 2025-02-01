27d75b442b25b14e1d2dfcee842ba90fa7411087
dtian118 <dziyangtian@gmail.com>
2025-02-01 02:35:53 -0500

    bugfix on chromdb

--- backend.py
+++ backend.py
@@ -11,6 +11,7 @@ from psycopg2.extras import RealDictCursor
 from typing import List, Optional
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
@@ -40,7 +41,13 @@ llm = ChatOpenAI(model=model)
 
 # LangChain setup
 embedding_function = OpenAIEmbeddings()
-vectorstore = chromadb.HttpClient(host=os.environ.get("CHROMADB_HOST"), port=os.environ.get("CHROMADB_PORT"))
+chroma_client = chromadb.HttpClient(host=os.environ.get("CHROMADB_HOST"), port=os.environ.get("CHROMADB_PORT"))
+collection = chroma_client.get_or_create_collection("langchain")
+vectorstore = Chroma(
+            client=chroma_client,
+            collection_name="langchain",
+            embedding_function=embedding_function,
+)
 
 storage_account_sas_url = os.environ.get("AZURE_STORAGE_SAS_URL")
 storage_container_name = os.environ.get("AZURE_STORAGE_CONTAINER")
