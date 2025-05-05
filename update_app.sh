72a89c9ddcc283c9d9a7b589fe59a4d92f8be4fe
Taha Noman <127983632+tahanoman@users.noreply.github.com>
2025-05-05 19:02:28 +0500

    Update update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -8,18 +8,18 @@ echo "Updating Python application on VM..."
 APP_DIR="/home/azureuser/SDA-Bootcamp-T"
 REPO_URL="github.com/tahanoman/SDA-Bootcamp-T.git"
 BRANCH="stage-6"
-GITHUB_TOKEN="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"
+GITHUB_TOKEN=$GITHUB_TOKEN
 
 # Update code
 if [ -d "$APP_DIR" ]; then
     sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
 else
-    sudo -u azureuser git clone -b $BRANCH "https://${GITHUB_TOKEN}@${REPO_URL}"
+    sudo -u azureuser git clone -b $BRANCH "https://${GITHUB_TOKEN}@${REPO_URL}" "$APP_DIR"
     sudo -u azureuser bash -c "cd $APP_DIR"
 fi
 
 # Install dependencies
-sudo -u azureuser /home/azureuser/minicondai3/envs/myenv2/bin/pip install --upgrade pip
+sudo -u azureuser /home/azureuser/miniconda3/envs/myenv2/bin/pip install --upgrade pip
 sudo -u azureuser /home/azureuser/miniconda3/envs/myenv2/bin/pip install -r ${APP_DIR}/requirements.txt
 
 # Restart the service
